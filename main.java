addb398150cb00c62e1810546dd944d924222bf3
shaniakarim <156203203+shaniakarim@users.noreply.github.com>
2024-06-16 01:55:18 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main.main;
+
+/**
+ *
+ * @author Darth Shania
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
